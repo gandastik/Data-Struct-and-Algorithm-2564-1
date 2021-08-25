ed6f3ddd2c14880ca36d8ab425f695c6bf66aed2
gandastik <gandastik@gmail.com>
2021-08-25 22:07:13 +0700

    correct 1 and 5

--- Lab2/1.py
+++ Lab2/1.py
@@ -2,22 +2,19 @@ class Calculator :
     def __init__(self, val: int):
         self.val = val
 
-    def __add__(self, a: int, b: int):
-        return a + b
+    def __add__(self, n):
+        return self.val + n.val
 
-    def __sub__(self, a: int, b: int):
-        return a-b
+    def __sub__(self, n):
+        return self.val - n.val
 
-    def __mul__(self, a: int, b: int):
-        return a*b
+    def __mul__(self, n):
+        return self.val * n.val
 
-    def __truediv__(self, a: int, b: int):
-        return a/b
+    def __truediv__(self, n):
+        return self.val / n.val
         
 x,y = input("Enter num1 num2 : ").split(",")
 
 x,y = Calculator(int(x)),Calculator(int(y))
-print(x.__add__(x.val, y.val))
-print(x.__sub__(x.val, y.val))
-print(x.__mul__(x.val, y.val))
-print(x.__truediv__(x.val, y.val))
+print(x+y,x-y,x*y,x/y,sep = "\n")
--- Lab2/5.py
+++ Lab2/5.py
@@ -1,40 +1,48 @@
+class GameTorKham:
+    def __init__(self, inputs):
+        self.inputs = inputs
+        self.lst = []
+        self.curr = []
+
+    def play(self):
+        for i in self.inputs:
+            self.lst.append(i.split(" "))
+
+        for i in self.lst:
+            if(len(i) == 1):
+                if(i[0] == 'R'):
+                    print("game restarted")
+                    self.curr = []
+                elif(i[0] == 'X'):
+                    break
+            #P mode
+            else:
+                if(i[0] == "P"):
+                    if(len(self.curr) != 0):
+                        lastTwoChar = self.curr[-1][-2:]
+                        #can be continue
+                        if(i[1][0].lower() == lastTwoChar[0].lower() and i[1][1].lower() == lastTwoChar[1].lower()):
+                            #append the word to the list
+                            self.curr.append(i[1])
+                            print(f"'{i[1]}' -> [", end="")
+                            for i in range(len(self.curr)):
+                                if(i != len(self.curr) - 1):
+                                    print(f"'{self.curr[i]}', ", end="")
+                                else:
+                                    print(f"'{self.curr[i]}']")
+                        #cannot be continue
+                        else:
+                            print(f"'{i[1]}' -> game over")
+                            break
+                    else:
+                        self.curr.append(i[1])
+                        print(f"'{i[1]}' -> ['{i[1]}']")
+                elif(i[0] == "p"):
+                    print(f"'{i[0]} {i[1]}' is Invalid Input !!!")
+                    break
+
 print("*** TorKham HanSaa ***")
 print("Enter Input : ", end="")
 inputs = [str(x) for x in input().split(',')]
-lst = []
-curr = []
-for i in inputs:
-    lst.append(i.split(" "))
-
-for i in lst:
-    if(len(i) == 1):
-        if(i[0] == 'R'):
-            print("game restarted")
-            curr = []
-        elif(i[0] == 'X'):
-            break
-    #P mode
-    else:
-        if(i[0] == "P"):
-            if(len(curr) != 0):
-                lastTwoChar = curr[-1][-2:]
-                #can be continue
-                if(i[1][0].lower() == lastTwoChar[0].lower() and i[1][1].lower() == lastTwoChar[1].lower()):
-                    #append the word to the list
-                    curr.append(i[1])
-                    print(f"'{i[1]}' -> [", end="")
-                    for i in range(len(curr)):
-                        if(i != len(curr) - 1):
-                            print(f"'{curr[i]}', ", end="")
-                        else:
-                            print(f"'{curr[i]}']")
-                #cannot be continue
-                else:
-                    print(f"'{i[1]}' -> game over")
-                    break
-            else:
-                curr.append(i[1])
-                print(f"'{i[1]}' -> ['{i[1]}']")
-        elif(i[0] == "p"):
-            print(f"'{i[0]} {i[1]}' is Invalid Input !!!")
-            break
+x = GameTorKham(inputs)
+x.play()
