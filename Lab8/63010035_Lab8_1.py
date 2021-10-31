f681eb99cfabfe82dc016ecfbb6ad9ea4c5d94c7
gandastik <gandastik@gmail.com>
2021-10-31 11:05:59 +0700

    completed lab8 problem 1

--- Lab8/63010035_Lab8_1.py
+++ Lab8/63010035_Lab8_1.py
@@ -12,6 +12,19 @@ class Tree:
     def __init__(self, root=None):
         self.root = root
 
+    def inOrder(self, root, dic, encoded):
+        if(root.left is None and root.right is None):
+            dic.update({root.symbol : encoded})
+        else:
+            if(root.right is not None):
+                encoded += '1'
+                self.inOrder(root.right, dic, encoded)
+            encoded = encoded[:-1]
+            if(root.left is not None):
+                encoded += '0'
+                self.inOrder(root.left, dic, encoded)
+        return dic
+
     def printTree(self, node, level = 0):
         if node != None:
             self.printTree(node.right, level + 1)
@@ -29,22 +42,30 @@ for i in word:
 items = sorted(dic.items())
 dic = dict(items)
 dic = dict(sorted(dic.items(), key=lambda item: item[1]))
-print(dic)
+# print(dic)
 nodes = [Node(dic[x], x) for x in dic.keys()]
 while(len(nodes) > 1):
-    #sort the nodes based on their frequency
     check = False
-    nodes = sorted(nodes, key=lambda x: x.freq)
+    #sort the nodes based on their frequency and symbol
+    nodes = sorted(nodes, key=lambda x: (x.freq, x.symbol))
     left = nodes[0]
     right = nodes[1]
+    if(left.symbol == ' ' and right.symbol == '*'):
+        left = nodes.pop(0)
+        nodes.append(left)
+        left = nodes[0]
+        right = nodes[1]
     newNode = Node(freq=left.freq+right.freq, left=left, right=right)
     nodes.remove(left)
     nodes.remove(right)
-    if(nodes[-1].freq == newNode.freq):
-        nodes.append(newNode)
-    else:
-        nodes.insert(1, newNode)
+    nodes.append(newNode)
 
 
 root = Tree(nodes[0])
+encodedTable = root.inOrder(nodes[0], {}, '')
+print(encodedTable)
 root.printTree(nodes[0])
+encodedWord = ''
+for i in word:
+    encodedWord += encodedTable[i]
+print(f"Encoded! : {encodedWord}")
